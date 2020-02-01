110b714c5f32d371e32e01942ba70dcbaba33b8e
Takashi SASAKI <takashi316@gmail.com>
2020-02-01 17:10:15 +0900

    trying to make pip package

---
+++ Makefile
@@ -0,0 +1,3 @@
+test:
+	sudo python3 setup.py -n egg_info
+
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+#with io.open('README.rst', 'rt', encoding='utf8') as f:
+#    readme = f.read()
+
+setup(
+    name='rename-to-iso8901',
+    version='0.0.1',
+    url='https://github.com/TakashiSasaki/rename-to-iso8901',
+    license='MIT',
+    maintainer='Takashi SASAKI',
+    maintainer_email='takashi316@gmail.com',
+    description=
+    'rename-to-iso8901 renames a file to its last modified date and time in ISO8901 style.',
+)
